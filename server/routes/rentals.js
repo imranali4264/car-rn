40432782383e2f57968b3499f1eeec8b62e5a49f
imran ali <imranalishaikh94@yhaoo.com>
2019-01-24 02:44:23 +0500

    add create rental functionality to server side and search rental by city

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,21 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const Rental = require("../models/Rental");
+const { mongoErrors } = require("../helpers/mongoose");
 const UserControl = require("../controllers/user");
+const User = require("../models/User");
 
 //get curent user by jwt token
 router.get("/secret", UserControl.loginMiddleware, (req, res) => {
   res.json({ secret: true });
 });
-//get all rentals
-router.get("/", (req, res) => {
-  Rental.find({})
-    .select("-bookings")
-    .exec((err, foundRental) => {
-      res.json(foundRental);
-    });
-});
-
 //get rental by id
 router.get("/:id", (req, res) => {
   const rentalId = req.params.id;
@@ -32,6 +25,68 @@ router.get("/:id", (req, res) => {
         });
       }
       return res.json(foundRental);
+    });
+});
+
+router.post("", UserControl.loginMiddleware, (req, res) => {
+  const {
+    title,
+    city,
+    color,
+    type,
+    shared,
+    description,
+    image,
+    hourRate
+  } = req.body;
+  const user = res.locals.user;
+
+  const rental = new Rental({
+    title,
+    city,
+    color,
+    type,
+    shared,
+    description,
+    image,
+    hourRate
+  });
+  rental.user = user.userId;
+  Rental.create(rental, (err, newRental) => {
+    if (err) {
+      res.status(422).send({ errors: mongoErrors(err.errors) });
+    }
+    User.updateOne(
+      { _id: user.userId },
+      { $push: { rentals: newRental } },
+      () => {}
+    );
+    return res.json(newRental);
+  });
+});
+
+//get all rentals and search rentals with query for city
+router.get("/", (req, res) => {
+  const city = req.query.city;
+  const query = city ? { city: city.toLowerCase() } : {};
+
+  Rental.find(query)
+    .select("-bookings")
+    .exec((err, foundRentals) => {
+      if (err) {
+        return res.status(422).send({ errors: mongoErrors(err.errors) });
+      }
+      if (city && foundRentals.length === 0) {
+        return res.status(404).send({
+          errors: [
+            {
+              title: "No rentals found",
+              detail: `There is no rental for this city ${city}`
+            }
+          ]
+        });
+      }
+      res.json(foundRentals);
     });
 });
 
